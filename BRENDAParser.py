d1791325eaf0b3b01b4fbe3d833cc952a42159e0
Thomas-McNulty <71293999+Thomas-McNulty@users.noreply.github.com>
2020-10-06 00:24:10 -0400

    Update BRENDAParser.py

--- BRENDAParser.py
+++ BRENDAParser.py
@@ -613,7 +613,7 @@ def commentStack(entry, startIndex):
 	return entry, comment
 
 
-#run abd time
+#run and time
 start_time = time.time()
 main()
 print("--- %s seconds ---" % (time.time() - start_time))
