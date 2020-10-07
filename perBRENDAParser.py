bf89be209ae26a76ad9cd016aab608e3a03c5ba9
Thomas-McNulty <71293999+Thomas-McNulty@users.noreply.github.com>
2020-10-06 20:59:04 -0400

    Update and rename BRENDAParser.py to perBRENDAParser.py

--- perBRENDAParser.py
+++ perBRENDAParser.py
@@ -1,14 +1,19 @@
 # 
 #
-#                       BRENDAEParser.py
-#  
-#       Authors:  Thomas McNulty 
+#                       perBRENDAEParser.py
+# 
 #       Date:     9/28/20
 #  
-#       summary
+#      PER BRENDA: Parse and Extract tool for Machine Learning applications using the BRENDA Database 
+#
+# 	Gian Marco Visani1,&, Thomas McNulty1,&, and Soha Hassoun1,2,* 
+#
+# 	1 Department of Computer Science, Tufts University, 161 College Ave, Medford, MA, 02155, USA 
+#
+# 	2 Department of Chemical and Biological Engineering, Tufts University, 4 Colby St, Medford, MA, 02155, USA 
+#
+# 	&Equal contribution. 
 #  
-#       Parser script. Accepts a texfile and converts it into
-#       JSON format similar to the BRENDA database. 
 #  
 #  
 
